a4a585bd677e61273766bc622525b0eec17278eb
Remato <renatobcostaa@gmail.com>
2021-03-11 11:51:30 -0300

    add base crawler configs

---
+++ configs.py
@@ -0,0 +1,16 @@
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+}
+
+links = [
+    'https://globalsportsarchive.com/',
+    'https://footystats.org/',
+    'https://fbref.com/',
+    'https://br.soccerway.com/',
+    'https://www.infogol.net/',
+    'https://www.goal.com/br/',
+    'https://pt.wikipedia.org/',
+    'https://www.ogol.com.br/',
+    'https://www.transfermarkt.com.br/',
+    'https://www.sofascore.com/'
+]
