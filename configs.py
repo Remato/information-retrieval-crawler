a16f1e9ddd5058423b3c44acd42504912cef7d8c
Remato <renatobcostaa@gmail.com>
2021-03-19 22:32:35 -0300

    add heuristic and new links

--- configs.py
+++ configs.py
@@ -1,16 +1,51 @@
-headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+agent = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; Trident/7.0; rv:11.0) like Gecko'
 }
 
+heuristic = [
+  {
+    'key': 'jogador',
+    'weight': 0.9
+  },
+  {
+    'key': 'player',
+    'weight': 0.8
+  },
+  {
+    'key': 'futebol',
+    'weight': 0.7
+  },
+  {
+    'key': 'soccer',
+    'weight': 0.6
+  },
+  {
+    'key': 'football',
+    'weight': 0.5
+  }, 
+  {
+    'key': 'perfil',
+    'weight': 0.5
+  },
+  {
+    'key': 'profile',
+    'weight': 0.4
+  },
+  {
+    'key': 'people',
+    'weight': 0.3
+  },
+]
+
 links = [
-    'https://globalsportsarchive.com/',
-    'https://footystats.org/',
-    'https://fbref.com/',
-    'https://br.soccerway.com/',
-    'https://www.infogol.net/',
-    'https://www.goal.com/br/',
-    'https://pt.wikipedia.org/',
-    'https://www.ogol.com.br/',
-    'https://www.transfermarkt.com.br/',
-    'https://www.sofascore.com/'
+    'https://globalsportsarchive.com',
+    'https://www.playmakerstats.com',
+    'https://fbref.com',
+    'https://br.soccerway.com',
+    'https://www.goal.com/br',
+    'https://www.ogol.com.br',
+    'https://www.transfermarkt.com.br',
+    'https://www.sambafoot.com/br',
+    'https://www.foxsports.com',
+    'https://www.espn.com.br'
 ]
