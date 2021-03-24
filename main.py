c790468fd806a567018a79cebeb63feae779b579
Remato <renatobcostaa@gmail.com>
2021-03-24 02:07:39 -0300

    comment main script

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import os
 import re
 import configs
-import numpy as np
+from threading import Thread
 
 # roda as threads do crawler para pegar os links
-#os.system("python3 crawler.py > out.py")
+# os.system("python3 crawler.py > out.py")
 
 import out
 
-visited_links = len(out.crawled_links)
-relevant_links = {}
+# visited_links = len(out.crawled_links)
+# relevant_links = {}
+# relevantFrontier = {}
 
-# cria heuristica para links
-for l in out.crawled_links:
-  value = 0
-  for heu in configs.heuristic:
-    if heu['key'] in l:
-      value += heu['weight']
-  if value > 0:
-    relevant_links[l] = value*100
+# # cria heuristica para links
+# for l in out.crawled_links:
+#   value = 0
+#   for heu in configs.heuristic:
+#     if heu['key'] in l:
+#       value += heu['weight']
+#   if value > 0:
+#     relevant_links[l] = value*100
 
-sorted_links = sorted(relevant_links.items(), key=lambda x: x[1], reverse=True)
-print(sorted_links)
-print(len(sorted_links), visited_links)
+# sorted_links = sorted(relevant_links.items(), key=lambda x: x[1], reverse=True)
+# print(sorted_links)
+# print(len(sorted_links), visited_links)
