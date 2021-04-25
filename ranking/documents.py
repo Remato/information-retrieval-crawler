c2806954094effc928e607df2f048f5c6c19486b
Remato <renatobcostaa@gmail.com>
2021-04-24 21:30:05 -0300

    add inverted index and docs

---
+++ ranking/documents.py
@@ -0,0 +1,18 @@
+docs = {
+  1: {
+    'title': 'um',
+    'body': 'jogador jogador jogador jogador 1'
+  },
+  2: {
+    'title': 'dois',
+    'body': 'futebol futebol messi 2 jogador'
+  },
+  3: {
+    'title': 'tres',
+    'body': ' futebol string grande do site 3 futebol'
+  },
+  4: {
+    'title': 'quatro',
+    'body': 'messi fez bastante gols no jogo de futebol'
+  }
+}
---
+++ ranking/invertedIndex.py
@@ -0,0 +1,6 @@
+indexes = {
+  'jogador': [1,2],
+  'futebol': [2,3],
+  'perfil': [1,3],
+  'players': [4,5,8]
+}
